4e018a2786f6f2939277c3857a6c83a8897a25a7
JJoas103 <kyumi1701@gmail.com>
2026-08-17 21:14:00 +0900

    db하드코딩 개선

--- src/main/java/db/BaseDAO.java
+++ src/main/java/db/BaseDAO.java
@@ -1,16 +1,26 @@
 package db;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 public abstract class BaseDAO {
-    protected String url = "jdbc:mysql://localhost:3306/yuls";
-    protected String user = "root";
-    protected String pass = "sukyum1003.";
+    private static final Properties props = new Properties();
+
+    static {
+        try (InputStream in = BaseDAO.class.getResourceAsStream("/db.properties")) {
+            props.load(in);
+        } catch (Exception e) {
+            throw new RuntimeException("DB 설정 파일을 읽을 수 없습니다", e);
+        }
+    }
 
     public Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection =  DriverManager.getConnection(url, user, pass);
-        return connection;
+        return DriverManager.getConnection(
+            props.getProperty("db.url"),
+            props.getProperty("db.user"),
+            props.getProperty("db.pass"));
     }
 }
